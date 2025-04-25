a3421fb7d6b52f87ab5e0e3f41ea545a9bcfa384
KRANKY-LUH <chocolataulait258@gmail.com>
2025-04-25 23:12:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def download():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
